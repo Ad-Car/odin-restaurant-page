3d1b41bee399a4b43b69e471c877b52b170d1598
Ad-Car <49081037+Ad-Car@users.noreply.github.com>
2024-12-16 00:54:05 +0000

    Complete webpack setup

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+
+import image1 from "./image1.jpg";
+
+const image = document.createElement("img");
+image.src = image1;
+console.log("Hello!!");
+
+document.body.appendChild(image);
